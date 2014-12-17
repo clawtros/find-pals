7ba9089c5b303fa254b3f3f7bacd8042d3d1a63d
Adam Benzan <adam.benzan@gmail.com>
2014-12-17 10:00:35 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+/*global require, __dirname */
+
+var express = require('express'),
+    app = require('express')(),
+    http = require('http').Server(app),
+    io = require('socket.io')(http),
+    locations = {},
+    handlebars = require('handlebars'),
+    fs = require('fs'),
+    mapTemplate = handlebars.compile(fs.readFileSync('./map.hbs', 'utf-8')),
+    PORT = 9999;
+
+
+app.get('/:room_id/', function(req, res) {
+    res.send(mapTemplate({ room: req.params.room_id }));
+});
+
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.use("/media", express.static(__dirname + '/media'));
+
+io.on('connection', function(socket){
+    socket.on('pos', function(newpos) {
+        locations[socket.id] = newpos;
+        socket.emit('positions updated', locations);
+    });
+    
+    socket.on('disconnect', function() {
+        io.emit('remove position', socket.id);
+        delete locations[socket.id];
+    });
+});
+
+http.listen(PORT, function(){
+    console.log('listening on *: ' + PORT);
+});
---
+++ media/scripts/map.js
@@ -0,0 +1,81 @@
+/*global module, google, setInterval */
+
+var Meet = function(options) {
+    this.options = options;
+    this.socket = options.socket;
+    this.markers = {};
+};
+
+Meet.prototype = {
+    lastPos: undefined,
+    start: function() {
+        var self = this;
+        
+	var mapOptions = {
+	    zoom: 14
+	};
+	this.map = new google.maps.Map(document.getElementById('map'),
+				       mapOptions);
+        this.updateLocation(true);
+        this.socket.on('positions updated', function(positions) {
+            for (var marker_id in self.markers) {
+                // update
+                var pos = positions[marker_id],
+                    latlng = new google.maps.LatLng(pos.k, pos.D);
+
+                if (self.markers[marker_id] !== undefined) {                    
+                    self.markers[marker_id].marker.setPosition(latlng);
+                    delete positions[marker_id];
+                }
+            }
+            
+            // add remaining
+            for (marker_id in positions) {
+                var pos = positions[marker_id],
+                    latlng = new google.maps.LatLng(pos.k, pos.D);
+                self.markers[marker_id] = {
+                    marker: new google.maps.Marker(
+                        {
+                            map: self.map,
+                            position: latlng
+                        })
+                };
+            }
+        });
+
+        this.socket.on('remove position', function(position_id) {
+            self.markers[position_id].marker.setMap(null);
+            delete self.markers[position_id];
+        });
+        
+        this.interval = setInterval((function(scope) {
+            return function() { scope.updateLocation.call(scope, false); };
+        })(this), 3000);
+    },
+    
+    updateLocation: function(should_pan) {
+        var self = this;
+        this.getPosition(function(position) {
+            var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            if (should_pan === true) {
+                self.map.panTo(latlng);
+            }
+
+
+            if (!self.lastPos) {
+                self.socket.emit('pos', latlng);
+            } else {
+                var distance = google.maps.geometry.spherical
+                        .computeDistanceBetween(self.lastPos, latlng);
+                if (distance > 5) {
+                    self.socket.emit('pos', latlng);
+                }
+            }
+            self.lastPos = latlng;
+        });
+    },
+    
+    getPosition: function(callback) {
+        navigator.geolocation.getCurrentPosition(callback);
+    }
+};
